0a20c93047b38262994983beca070013117a1323
Aaron <gabbert.aaron@gmail.com>
2024-09-18 05:45:13 -0500

    Added DropdownLinkModal

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,9 @@
-import Button from "./Button.svelte";
-import IocList from "./IocList.svelte";
-import Item from "./Item.svelte";
-import Sidebar from "./Sidebar.svelte";
+import Button from "./components/Button.svelte";
+import IocList from "./components/IocList.svelte";
+import Item from "./components/Item.svelte";
+import Sidebar from "./components/Sidebar.svelte";
+import DropdownLinkModal from "./components/DropdownLinkModal.svelte";
 
-export * from "./sidebarSvelte.js";
-export { Button, IocList, Item, Sidebar };
+export * from "./obsidian/sidebarSvelte.js";
+export * from "./obsidian/dropdownLinkModal.js";
+export { Button, DropdownLinkModal, IocList, Item, Sidebar };
