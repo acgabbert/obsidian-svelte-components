818dee0376b86150f2020941f08feef0d9e6e60f
Aaron <gabbert.aaron@gmail.com>
2024-09-19 09:36:14 -0500

    removed unneeded var inits

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -3,8 +3,6 @@
     import IocList from "./IocList.svelte";
     //export let iocList: string[];
     export let indicators: ParsedIndicators[];
-    const iocListTitles = ["IPs", "Domains", "Hashes"];
-    const iocs = ["8.8.8.8", "9.9.9.9"];
 </script>
 
 <h4>Extracted Indicators</h4>
