d68ab7ce27da6f79e298145b417a34be34278870
Aaron <gabbert.aaron@gmail.com>
2024-10-03 05:41:14 -0500

    comment

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,4 +7,5 @@ import DropdownSelector from "./components/DropdownSelector.svelte";
 export * from "./obsidian/sidebarSvelte.ts";
 export * from "./obsidian/dropdownLinkModal.ts";
 export * from "./obsidian/settingCollectionModal.ts";
+// explicit export of Svelte components
 export { Button, DropdownSelector, IocList, Item, Sidebar };
