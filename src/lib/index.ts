0a89949123b7b2bdd2e1b045cb165a3497121f23
acgabbert <55032726+acgabbert@users.noreply.github.com>
2024-10-03 13:39:17 -0500

    Fixed exports, again

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,9 +3,10 @@ import IocList from "./components/IocList.svelte";
 import Item from "./components/Item.svelte";
 import Sidebar from "./components/Sidebar.svelte";
 import DropdownSelector from "./components/DropdownSelector.svelte";
+import SettingCollection from "./components/SettingCollection.svelte";
 
 export * from "./obsidian/sidebarSvelte.js";
 export * from "./obsidian/dropdownLinkModal.js";
 export * from "./obsidian/settingCollectionModal.js";
 // explicit export of Svelte components
-export { Button, DropdownSelector, IocList, Item, Sidebar };
+export { Button, DropdownSelector, IocList, Item, SettingCollection, Sidebar };
