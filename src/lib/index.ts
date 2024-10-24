b5e1af61bd9ec29a3a6c2b24c2fa1fd22ef7ae2e
Aaron <gabbert.aaron@gmail.com>
2024-10-24 10:32:43 -0500

    fixed exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ import Button from "./components/Button.svelte";
 import IocList from "./components/IocList.svelte";
 import Item from "./components/Item.svelte";
 import LoadingCode from "./components/LoadingCode.svelte";
+import LoadingMarkdown from "./components/LoadingMarkdown.svelte";
 import Sidebar from "./components/Sidebar.svelte";
 import DropdownSelector from "./components/DropdownSelector.svelte";
 import SettingCollection from "./components/SettingCollection.svelte";
@@ -11,4 +12,4 @@ export * from "./obsidian/dropdownLinkModal.js";
 export * from "./obsidian/settingCollectionModal.js";
 export * from "./obsidian/sidebarSvelte.js";
 // explicit export of Svelte components
-export { Button, DropdownSelector, IocList, Item, LoadingCode, SettingCollection, Sidebar };
+export { Button, DropdownSelector, IocList, Item, LoadingCode, LoadingMarkdown, SettingCollection, Sidebar };
