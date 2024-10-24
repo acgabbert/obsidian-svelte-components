3a9c91cad465d9b7caa16859c0441e1ebfbf17d8
Aaron <gabbert.aaron@gmail.com>
2024-10-24 10:41:43 -0500

    trying to fix deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	build: {
-		rollupOptions: {
-			external: ['marked']
-		}
+	optimizeDeps: {
+		include: ['marked']
 	}
 });
