86ea143dd98a672b1f03b7741073c1613dc891a0
acgabbert <aaron@gabbert.me>
2024-12-02 15:56:42 -0600

    explicit re-export of all components. updated deps

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,12 @@
 import Button from "./components/Button.svelte";
+import DropdownSelector from "./components/DropdownSelector.svelte";
 import IocList from "./components/IocList.svelte";
 import Item from "./components/Item.svelte";
 import LoadingCode from "./components/LoadingCode.svelte";
 import LoadingMarkdown from "./components/LoadingMarkdown.svelte";
+import OcrIocList from "./components/OcrIocList.svelte";
+import SearchButton from "./components/SearchButton.svelte";
 import Sidebar from "./components/Sidebar.svelte";
-import DropdownSelector from "./components/DropdownSelector.svelte";
 import SettingCollection from "./components/SettingCollection.svelte";
 
 export * from "./obsidian/loadingModal.js";
@@ -12,4 +14,15 @@ export * from "./obsidian/dropdownLinkModal.js";
 export * from "./obsidian/settingCollectionModal.js";
 export * from "./obsidian/sidebar.js";
 // explicit export of Svelte components
-export { Button, DropdownSelector, IocList, Item, LoadingCode, LoadingMarkdown, SettingCollection, Sidebar };
+export {
+    Button,
+    DropdownSelector,
+    IocList,
+    Item,
+    LoadingCode,
+    LoadingMarkdown,
+    OcrIocList,
+    SearchButton,
+    SettingCollection,
+    Sidebar
+};
