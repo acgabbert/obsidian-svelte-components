43847f441d066f2737a961615e36ded9fd05b847
acgabbert <aaron@gabbert.me>
2025-02-27 10:28:44 -0600

    added class to sidebar for easier manipulation of DOM

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -5,18 +5,20 @@
     $: hasIndicators = indicators.some(list => list.items.length > 0);
 </script>
 
-<h4>Extracted Indicators</h4>
-{#if hasIndicators}
-    {#each indicators as indicatorList}
-        {#if indicatorList.items.length > 0}
-            <IocList indicatorList={indicatorList}/>
-        {/if}
-    {/each}
-{:else}
-    <div class="empty-state">
-        No indicators found.
-    </div>
-{/if}
+<div class="indicator-sidebar-container">
+    <h4>Extracted Indicators</h4>
+    {#if hasIndicators}
+        {#each indicators as indicatorList}
+            {#if indicatorList.items.length > 0}
+                <IocList indicatorList={indicatorList}/>
+            {/if}
+        {/each}
+    {:else}
+        <div class="empty-state">
+            No indicators found.
+        </div>
+    {/if}
+</div>
 
 <style>
     .empty-state {
